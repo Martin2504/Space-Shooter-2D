fb27a7cdc6d476184de0653c62ae65acdb4d361a
Martin2504 <58813528+Martin2504@users.noreply.github.com>
2023-04-10 15:01:15 +0100

    Added Game Over Text upon player death

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -16,11 +16,14 @@ public class UIManager : MonoBehaviour
     // Referene to the lives sprites
     [SerializeField]
     private Sprite[] _liveSprites;
+    [SerializeField]
+    private Text _gameOverText;
 
     // Start is called before the first frame update
     void Start()
     {
         _scoreText.text = "Score: " + 0;
+        _gameOverText.gameObject.SetActive(false);
     }
 
     public void UpdateScore(int playerScore)
@@ -31,7 +34,13 @@ public class UIManager : MonoBehaviour
     public void UpdateLives(int livesLeft)
     {
         _LivesImg.sprite = _liveSprites[livesLeft];     // change the sprite in the image.
+
+        if (livesLeft == 0)
+        {
+            _gameOverText.gameObject.SetActive(true);   // Display Game Over text.
+        }   
     }
 
+    
 
 }
